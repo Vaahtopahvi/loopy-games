74d04a9726d1124a7137ccc4e73ac23d8a12a974
vaahtopahvi <kermakahvi@hotmail.com>
2025-08-15 02:57:23 +0300

    update data and keypair values

--- src/types/Game.ts
+++ src/types/Game.ts
@@ -1,13 +1,16 @@
 export type Game = {
   id: string;
   title: string;
-  genres: string[];
-  platforms: string; // String[] if multiple platforms
+  genre: string;
+  platform: string; // String[] if multiple platforms
   completionDate: string; // ISO date string
-  hoursPlayed: number;
+  playtime: string; // e.g., "On-going" or "2024-02-28"
   rating: number; // 0-5 stars
   review: string;
-  interestingFact?: string; //questionmark means this field is optional
-  imageUrl?: string; // URL to the game's image
+  interestingFact: string; //questionmark means this field is optional
+  coverImage?: string; // URL to the game's image
+  gameType: "story" | "endless";
   recommended: boolean; // true if the game is recommended
+  isOngoing?: boolean; // true if the game is still being played
+  completionist: boolean; // true if the game is completed
 };
