9f91c34e6b1fe241aee74cfc620652aad42dd46c
vaahtopahvi <kermakahvi@hotmail.com>
2025-08-15 03:03:51 +0300

    add more pages for future and fix Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { games } from "../data/games";
 import { GameCard } from "../components/GameCard";
 
-export default function Index() {
+export default function Home() {
   return (
     <div className="p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       {games.map((game) => (
