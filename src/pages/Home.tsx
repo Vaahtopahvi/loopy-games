b2aad14681d15cbed116defd6cef662969d810d3
vaahtopahvi <kermakahvi@hotmail.com>
2025-08-27 23:24:26 +0300

    fix some responsiveness

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,12 @@ import { GameCard } from "../components/GameCard";
 
 export default function Home() {
   return (
-    <div className="p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      {games.map((game) => (
-        <GameCard key={game.id} game={game} />
-      ))}
+    <div className="w-full px-4 2xl:max-w-5xl 2xl:mx-auto">
+      <div className="p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {games.map((game) => (
+          <GameCard key={game.id} game={game} />
+        ))}
+      </div>
     </div>
   );
 }
