ae6791a419445556379aa9f1892256ec7ef9a0cf
riku <riku.vaatanen@gmail.com>
2025-10-14 23:04:16 +0300

    small ui changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-900 text-white">
+    <div className="min-h-screen text-white">
       <Home />
     </div>
   );
