5e78bb061f03955ebc5a68e871ae7804c00b2079
riku <kermakahvi@hotmail.com>
2025-10-22 23:11:11 +0300

    update Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,15 @@
-import { games } from "../data/games";
+// src/pages/Home.tsx
+import { type Game } from "../types/Game";
 import { GameCard } from "../components/GameCard";
 
-export default function Home() {
+type HomeProps = {
+  games: Game[];
+};
+
+export default function Home({ games }: HomeProps) {
   return (
-    <div className="w-full px-4 2xl:max-w-5xl 2xl:mx-auto">
-      <div className="p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="w-full">
+      <div className="pt-5 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mx-auto">
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
