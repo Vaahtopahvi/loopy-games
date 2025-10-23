4ad8f1afe7cdde28478bc7df004de6199b422abd
riku <kermakahvi@hotmail.com>
2025-10-23 21:35:03 +0300

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 // import  { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { games as allGames } from "./data/games";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Container from "./components/Container";
 
+
 export default function App() {
+  const [filteredGames, setFilteredGames] = useState(allGames)
+  
   return (
     <div className="min-h-screen text-white">
-      <Header />
+      <Header games={allGames} onFilterChange={setFilteredGames} />
       <main>
         <Container className="py-6">
-          <Home />
+          <Home games={filteredGames} />
         </Container>
       </main>
     </div>
