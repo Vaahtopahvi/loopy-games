cab3b7cae2be2bcc8c04193c37ae47c6534ca234
riku <kermakahvi@hotmail.com>
2025-10-29 02:48:07 +0200

    update App for modal

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,27 @@ import { games as allGames } from "./data/games";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Container from "./components/Container";
+import Form from "./components/Form";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./components/ui/dialog";
 
 
 export default function App() {
   const [filteredGames, setFilteredGames] = useState(allGames)
+  const [isFormOpen, setIsFormOpen] = useState(true)
   
   return (
     <div className="min-h-screen text-white">
-      <Header games={allGames} onFilterChange={setFilteredGames} />
+      <Header games={allGames} onFilterChange={setFilteredGames} onAddGame={() => setIsFormOpen(true)} />
       <main>
         <Container className="py-6">
+          <Dialog open={isFormOpen} onOpenChange={setIsFormOpen}>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle className="text-white">Add New Game</DialogTitle>
+              </DialogHeader>
+              <Form />
+            </DialogContent>
+          </Dialog>
           <Home games={filteredGames} />
         </Container>
       </main>
