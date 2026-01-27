5edf92c1add6f9d20cffe9921dc1cb5aa29d078d
riku <kermakahvi@hotmail.com>
2026-01-27 04:19:41 +0200

    fixes for localhost

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,12 @@ export default defineConfig({
       "luuppi": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+      target: "http://localhost:3000",
+      changeOrigin: true,
+    },
+  },
+  },
 });
